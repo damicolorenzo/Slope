40c39e06a9877bd918034183355cde67524abaf5
damicolorenzo <lorenzo.damico2002@gmail.com>
2024-05-22 22:43:11 +0200

    Aggiunta di alcune classi in Entity

---
+++ app/Entity/EWeeklySkipass.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once("ESkipassBooking.php");
+
+class EDailySkipass extends ESkipassBooking {
+
+    //attributes
+    protected static float $priceMultiplier = 0.4;
+    protected static int $period = 7;
+
+    //constructor
+    public function __construct(string $name, string $surname, string $stringStartDate, string $type, string $email, float $totalSkiFacilitiesPrice) {
+        parent::__construct($name, $surname, $stringStartDate, $type, $email, $totalSkiFacilitiesPrice);
+    }
+
+    //Get and set methods
+    public function getPriceMultiplier() :float {return $this->priceMultiplier;}
+    public function getPeriod() :int {return $this->period;}
+}
+
+?>
